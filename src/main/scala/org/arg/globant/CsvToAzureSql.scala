bdbb2cbfa4b88ebb42f1618b01f91b50cd63d5e7
Alberto Gaytan  <argaytan@gmail.com>
2023-05-17 17:55:53 -0600

    extract csv data to a DF and load it to Azure SQL

--- src/main/scala/org/arg/globant/CsvToAzureSql.scala
+++ src/main/scala/org/arg/globant/CsvToAzureSql.scala
@@ -3,11 +3,13 @@ package org.arg.globant
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import com.arg.common.utilities.UtilFunctions
 import org.apache.log4j.Logger
+import java.util.Properties
 
 object CsvToAzureSql {
   def main(args: Array[String]): Unit = {
     val utilFunctions: UtilFunctions = new UtilFunctions()
-    val logger: Logger = Logger.getRootLogger()
+    val logger: Logger = Logger.getRootLogger
+    val props: Properties = getProperties
 
     logger.warn("Starting CsvToAzureSql Application")
     val spark = SparkSession.builder()
@@ -15,35 +17,69 @@ object CsvToAzureSql {
       .config(utilFunctions.getSparkConf)
       .getOrCreate()
 
+
     // Configure Azure SQL Server connection properties
-    val jdbcHostname = "testarg.database.windows.net"
-    val jdbcDatabase = "testdb"
-    val jdbcUrl = s"jdbc:sqlserver://$jdbcHostname:1433;database=$jdbcDatabase;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;authentication=ActiveDirectoryPassword"
-    val jdbcUsername = "dev1"
-    val jdbcPassword = "xxxxxx"  // todo add env variable
-    val jdbcDriver = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+    logger.warn("Configure Azure SQL Server connection properties")
+    val jdbcHostname = props.get("jdbcHostname").toString
+    val jdbcDatabase = props.get("jdbcDatabase").toString
+    val jdbcDriver = props.get("jdbcDriver").toString
+    val jdbcUsername = props.get("jdbcUser").toString
+    val jdbcPassword = props.get("jdbcPassword").toString
+    val jdbcUrl = s"jdbc:sqlserver://$jdbcHostname:1433;database=$jdbcDatabase;"
 
-    // Load CSV data into a DataFrame and add column names
-    val df = spark.read
-      .format("csv")
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .load("src/main/resources/hired_employees.csv")
-      .toDF("employee_id", "name", "date_hired", "department", "job")  // add column names
-    df.printSchema()
-    df.show(false)
 
-    // Write DataFrame to Azure SQL Server
-    df.write
-      .format("jdbc")
-      .mode(SaveMode.Append)
-      .option("url", jdbcUrl)
-      .option("dbtable", "common.employees")
-      .option("user", jdbcUsername)
-      .option("password", jdbcPassword)
-      .option("driver", jdbcDriver)
-      .save()
+    val connectionProperties: Properties = new Properties()
+    connectionProperties.setProperty("user", jdbcUsername)
+    connectionProperties.setProperty("password", jdbcPassword)
+    connectionProperties.setProperty("driver", jdbcDriver)
 
+    // Database Tables
+    val schema = "common"
+    val tableList = Map(
+      "hired_employees" -> "'employee_id', 'employee_name', 'date_hired', 'department_id', 'job_id'",
+      "jobs" -> "'job_id', 'job_name'",
+      "departments" -> "'department_id', 'name'"
+    )
+
+    // Iterate over the table list to overwrite data in Azure SQL Database
+    for ((tableName, f) <- tableList) {
+      println(s"table name: $tableName")
+      println(s"fields: $f")
+
+      //Create column name type variables
+      val fields = Seq(f.split(","): _*)
+
+      // Load CSV data into a DataFrame and add column names
+      logger.warn("Load CSV data into a DataFrame and add column names")
+      val df = spark.read
+        .format("csv")
+        .option("header", "false")
+        .option("inferSchema", "true")
+        .load("src/main/resources/" + tableName + ".csv")
+        .toDF(fields: _*)  // add column names
+      df.printSchema()
+      df.show(false)
+
+      // Write the DataFrame to the table inAzure SQL Database
+      logger.warn("Write the DataFrame to the table inAzure SQL Database")
+      val saveMode = SaveMode.Overwrite
+      df.write.mode(saveMode)
+        .jdbc(jdbcUrl, schema + "." + tableName, connectionProperties)
+
+    }
     spark.stop()
+
+  }
+
+
+  def getProperties: Properties = {
+    val prop: Properties = new Properties()
+    prop.put("jdbcHostname", sys.env.getOrElse("AZURE_SQL_URL", ""))
+    prop.put("jdbcDriver", sys.env.getOrElse("AZURE_SQL_DRIVER", ""))
+    prop.put("jdbcDatabase", sys.env.getOrElse("AZURE_SQL_DB", ""))
+    prop.put("jdbcUser", sys.env.getOrElse("AZURE_SQL_USER", ""))
+    prop.put("jdbcPassword", sys.env.getOrElse("AZURE_SQL_PASSWORD", ""))
+    prop
   }
 }
+
